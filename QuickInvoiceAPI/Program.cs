041a6f4ae3058f907e2f66876e4ad3119273dca2
YerlinLeal <yerlin.leal@ucr.ac.cr>
2024-03-18 11:38:17 -0600

    Ajustes SqlStringConnection

--- QuickInvoiceAPI/Program.cs
+++ QuickInvoiceAPI/Program.cs
@@ -46,11 +46,13 @@ builder.Services.AddCors
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+    
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(rulesCors);
 
